f5b1f864314063e14b9b9c967a08db8384bd09f6
Leinner-Lopez <leinner1006@gmail.com>
2025-09-20 14:21:04 -0500

    Vital-Care-Web

---
+++ src/main/resources/static/js/FuncionesAdministrador.js
@@ -0,0 +1,37 @@
+function functionAdministrador(link) {
+    const Editar = link.getAttribute("data-href-update");
+    const Eliminar = link.getAttribute("data-href-delete");
+    const confirmButton = document.getElementById("acceptConfirmButton");
+    const cancelButton = document.getElementById("cancelConfirmButton");
+    const model = document.getElementById("Model");
+    const fondoDialogo = document.getElementById("fondoDialogo");
+    const texto = document.getElementById("model-text");
+    if (Editar) {
+        fondoDialogo.style.display = 'block';
+        texto.textContent = '¿Estás seguro de Editar este Administrador?';
+        model.showModal();
+        confirmButton.onclick = function () {
+            fondoDialogo.style.display = 'none';
+            model.close();
+            window.location.href = Editar;
+        };
+        cancelButton.onclick = function () {
+            fondoDialogo.style.display = 'none';
+            model.close();
+        };
+    } else {
+        fondoDialogo.style.display = 'block';
+        texto.textContent = '¿Estás seguro de Eliminar este Administrador?';
+        model.showModal();
+        confirmButton.onclick = function () {
+            fondoDialogo.style.display = 'none';
+            model.close();
+            window.location.href = Eliminar;
+        };
+        cancelButton.onclick = function () {
+            fondoDialogo.style.display = 'none';
+            model.close();
+        };
+    }
+    return false;
+}

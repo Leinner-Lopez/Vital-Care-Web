c3d0bf112c725616cb37f231aa4742591b3d1eee
Leinner-Lopez <leinner1006@gmail.com>
2025-09-28 14:33:38 -0500

    Vital-Care-Web

--- src/main/java/com/leinner/springboot/vital_care/services/Impl/DisponibilidadServiceImpl.java
+++ src/main/java/com/leinner/springboot/vital_care/services/Impl/DisponibilidadServiceImpl.java
@@ -1,5 +1,7 @@
 package com.leinner.springboot.vital_care.services.Impl;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.leinner.springboot.vital_care.entities.Disponibilidad;
@@ -10,21 +12,23 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class DisponibilidadServiceImpl implements DisponibilidadService{
+public class DisponibilidadServiceImpl implements DisponibilidadService {
 
-    private final DisponibilidadRepository disponibilidadRepository; 
+    private final DisponibilidadRepository disponibilidadRepository;
 
     @Override
     public Disponibilidad agregarDisponibilidad(Long numeroDocumento) {
         Disponibilidad disponibilidad = new Disponibilidad();
         disponibilidad.setDocumentoMedico(numeroDocumento);
+        disponibilidad.setInicioDisponibilidad(LocalDateTime.of(2020, 1, 20, 00, 00));
+        disponibilidad.setFinDisponibilidad(LocalDateTime.of(2020, 2, 20, 00, 00));
         return disponibilidadRepository.save(disponibilidad);
     }
 
     @Override
-    public Disponibilidad actualizarDisponibilidad(Long numeroDocumento,Disponibilidad disponibilidad) {
+    public Disponibilidad actualizarDisponibilidad(Long numeroDocumento, Disponibilidad disponibilidad) {
         Disponibilidad disponibilidadExistente = disponibilidadRepository.findById(numeroDocumento).orElse(null);
-        if(disponibilidadExistente != null){
+        if (disponibilidadExistente != null) {
             disponibilidadExistente.setInicioDisponibilidad(disponibilidad.getInicioDisponibilidad());
             disponibilidadExistente.setFinDisponibilidad(disponibilidad.getFinDisponibilidad());
             return disponibilidadRepository.save(disponibilidadExistente);
@@ -36,5 +40,5 @@ public class DisponibilidadServiceImpl implements DisponibilidadService{
     public Disponibilidad obtenerDisponibilidadPorId(Long numeroDocumento) {
         return disponibilidadRepository.findById(numeroDocumento).orElse(null);
     }
-    
+
 }
